d50eaf0126c0b73a269329a3a05d4aeffcf4e222
Stephen Nguyen <stlnguyen@ucdavis.edu>
2018-03-15 17:32:16 -0700

    post working for car, still need to do part, also need to reset form on submit

--- client/components/cars.js
+++ client/components/cars.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
-import {logout, getAllCars} from '../store'
+import {logout, getAllCars, updateCar, writeCar} from '../store'
 
 
 
@@ -19,13 +19,46 @@ export class Cars extends Component {
             <div>
                 {this.props.cars && 
                 this.props.cars.map(car => (
-                    <div key={car.id} >
+                    car.id && (<div key={car.id} >
                         <h2> Car Model: {car.name} </h2>
                         <p> Description: {car.description} </p>
                         <h5> Views: {car.views} </h5>
-                    </div>
+                    </div>)
                 ))
                 }
+                 <form
+                    onSubmit={e =>
+                    this.props.onSubmit(this.props.newCar, e)
+                    }
+                    id="carform"
+                 >
+                    <input
+                    type="text"
+                    placeholder="Enter a name"
+                    name="name"
+                    onChange={e => this.props.onChange(this.props.newCar, e)}
+                    value={this.props.newCar.name}
+                    />
+                    <input
+                    type="text"
+                    placeholder="Enter a description"
+                    name="body"
+                    onChange={e => this.props.onChange(this.props.newCar, e)}
+                    value={this.props.newCar.description}
+                    />
+                    <h3>Category</h3>
+                    <select
+                    name="category"
+                    onChange={e => this.props.onChange(this.props.newCar, e)}
+                    value={this.props.newCar.category}
+                    >
+                    <option value="1">Car</option>
+                    <option value="2">Part</option>
+                    <option value="3">Other</option>
+                    </select>
+                    <br />
+                    <button type="Submit">Submit</button>
+                </form>
             </div>
         )}
 }
@@ -34,7 +67,8 @@ export class Cars extends Component {
 const mapState = state => {
     return {
       isLoggedIn: !!state.user.id,
-      cars: state.base.cars
+      cars: state.base.cars,
+      newCar: state.base.newCar
     }
   }
   
@@ -45,6 +79,46 @@ const mapState = state => {
       },
       getCars() {
         dispatch(getAllCars());
+      },
+      onChange(theCar, e) {
+        const updatedCar = theCar;
+        if (e.target.getAttribute("name") === "name") {
+            updatedCar.name = e.target.value;
+        }
+        if (e.target.getAttribute("name") === "body") {
+            updatedCar.description = e.target.value;
+        }
+        if (e.target.getAttribute("name") === "category") {
+            updatedCar.category = e.target.value;
+        }
+  
+        dispatch(updateCar(updatedCar));
+      },
+      onSubmit(theCar, e) {
+        e.preventDefault();
+        document.getElementById("carform").reset();
+        theCar.views = 0;
+        theCar.parentId = 0;
+        let promisePostCar = () => {
+            return new Promise(function(resolve,reject){
+                dispatch(writeCar(theCar))
+                resolve();
+            })
+        }
+        let promiseUpdateCar = () => {
+            return new Promise(function(resolve,reject){
+                dispatch(updateCar({}))
+                resolve();
+            })
+        }
+        let promiseGetAllCars = () => {
+            return new Promise(function(resolve,reject){
+                dispatch(getAllCars())
+                resolve();
+            })
+        }
+        promisePostCar().then(()=> promiseUpdateCar()).then(()=>promiseGetAllCars());
+       // document.getElementById("carform").reset();
       }
     }
   }
--- client/store/base.js
+++ client/store/base.js
@@ -6,20 +6,25 @@ import history from '../history'
  */
 const GET_CARS = 'GET_CARS'
 const GET_PARTS = 'GET_PARTS'
+const UPDATE_CAR = 'UPDATE_CAR'
+const POST_CAR = "POST_CAR";
 
 /**
  * INITIAL STATE
  */
 const initialState = {
     cars: [],
-    parts: []
+    parts: [],
+    newCar: {}
 }
 
 /**
  * ACTION CREATORS
  */
-const getCars = cars => ({type: GET_CARS, cars})
-const getParts = parts => ({type: GET_PARTS, parts})
+const getCars = cars => ({type: GET_CARS, cars});
+const getParts = parts => ({type: GET_PARTS, parts});
+const postCar = newCar => ({ type: POST_CAR, newCar });
+export const updateCar = newCar => ({type: UPDATE_CAR, newCar});
 
 /**
  * THUNK CREATORS
@@ -40,6 +45,20 @@ const getParts = parts => ({type: GET_PARTS, parts})
             dispatch(getParts(toDispatchParts));
         });
     };
+    export const writeCar = car => dispatch => {
+        axios
+          .post("/api/cars", {
+            description: car.description,
+            name: car.name,
+            classtypeId: 1,
+            parentId: car.parentId,
+            views: car.views
+          })
+          .then(() => {
+            dispatch(postCar(car));
+          })
+          .catch(err => console.error("Error", err));
+      };
 
 
 /**
@@ -51,6 +70,13 @@ export default function (state = initialState, action) {
     return Object.assign({}, state, { cars: action.cars });
     case GET_PARTS:
     return Object.assign({}, state, { parts: action.parts});
+    case UPDATE_CAR:
+    return Object.assign({}, state, { updateCar: action.newCar});
+    case POST_CAR:{
+        console.log("new car is" ,action.newCar)
+        return Object.assign({}, state, {cars: state.cars.concat(action.newCar)});
+    }
+ 
     default:
       return state
   }
